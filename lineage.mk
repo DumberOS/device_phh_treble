571af84baee5310f0a7baf02384e55775bb7b3eb
Michal Brzozowski <miki@extremic.com>
2025-06-11 09:22:55 +0200

    Fix bluetooth not turning on
    Change-Id: Ibbdd8abc068b911e5e30b1db599454c966e5bb35

--- lineage.mk
+++ lineage.mk
@@ -1,3 +1,8 @@
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
 -include vendor/lineage/build/core/config.mk
 -include vendor/lineage/build/core/apicheck.mk
+
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.bt.unsupported.commands=182
+
