506338a5e695f169fe3ca51e413d3c0e9b341768
Michal Brzozowski <miki@extremic.com>
2025-12-03 10:54:18 +0100

    Add missing build files
    Change-Id: I8ddb52e2d0318ff736498c2fe9499ebc49726b70

---
+++ lineage_arm64_bvN.mk
@@ -0,0 +1,46 @@
+TARGET_GAPPS_ARCH := arm64
+include build/make/target/product/aosp_arm64.mk
+$(call inherit-product, device/phh/treble/base.mk)
+
+
+$(call inherit-product, device/phh/treble/lineage.mk)
+
+PRODUCT_NAME := arm64_bvN
+PRODUCT_DEVICE := tdgsi_arm64_ab
+PRODUCT_BRAND := google
+PRODUCT_SYSTEM_BRAND := google
+PRODUCT_MODEL := Dumbdroid vanilla
+
+# Overwrite the inherited "emulator" characteristics
+PRODUCT_CHARACTERISTICS := device
+
+WITH_MICROG := true
+
+PRODUCT_PACKAGES += \
+    android.hardware.lights-service.example
+
+PRODUCT_REMOVE_PACKAGES := \
+AudioFX \
+BasicDreams \
+EasterEgg \
+Etar \
+ExactCalculator \
+Recorder \
+talkback \
+Velvet \
+Eleven \
+NotoColorEmojiLegacy.ttf \
+MtkInCallService \
+#Seedvault \
+
+PRODUCT_PACKAGES += \
+    NotoSansCJK-Regular.ttc
+
+PRODUCT_PACKAGES += package_filter
+
+PRODUCT_PACKAGES += GmsCore GsfProxy FakeStore
+
+#PRODUCT_PACKAGES += ImsApp
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.dumbdroid.branch=vanilla
---
+++ lineage_dv30.mk
@@ -0,0 +1,4 @@
+$(call inherit-product, device/phh/treble/lineage_arm64_bvN.mk)
+
+PRODUCT_EXTRA_VNDK_VERSIONS := 30
+
