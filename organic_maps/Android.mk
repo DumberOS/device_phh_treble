1664ade7c7903c612a0bf8ffefeb78cf86e07cb3
Michal Brzozowski <miki@extremic.com>
2026-03-17 18:41:01 +0100

    Reduce organic maps apk size
    Change-Id: If8077485f680615fada5e56c38949a1ebdd332cc

--- organic_maps/Android.mk
+++ organic_maps/Android.mk
@@ -12,7 +12,8 @@ LOCAL_MODULE_TAGS := optional
 LOCAL_SRC_FILES := organic_maps.apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := platform
+LOCAL_MULTILIB := first
+LOCAL_PREBUILT_JNI_LIBS_arm64 := @lib/arm64-v8a/liborganicmaps.so
 
 include $(BUILD_PREBUILT)
-
 
