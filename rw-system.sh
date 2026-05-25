2c7b452ef519d60ae437117fbe31cd36cd4f6e3d
Michal Brzozowski <miki@extremic.com>
2026-05-25 13:46:34 +0200

    Fix Dumber Mini DPI to 210
    Change-Id: Ice9e510c01ec1fadcab79fe35d09b0d64f2b6609

--- rw-system.sh
+++ rw-system.sh
@@ -66,6 +66,12 @@ fi
 
 setprop sys.usb.ffs.aio_compat true
 
+case "$(getprop ro.product.vendor.model)" in
+    "Dumber mini"|"S9")
+        resetprop_phh ro.sf.lcd_density 210
+        ;;
+esac
+
 if getprop ro.vendor.build.fingerprint | grep -q -i -e Blackview/BV9500Plus;then
     setprop persist.adb.nonblocking_ffs true
 else
